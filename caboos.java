ef4de90a4f68666d1bdba3b938b7cf0e5420e35a
Brian Poelstra <brianpoelstra@gmail.com>
2023-09-18 16:57:02 -0400

    Update caboos.java
    small update

--- caboos.java
+++ caboos.java
@@ -2,5 +2,9 @@ public class caboos extends Train{
     public caboos() {
         System.out.println("I am a caboos");
     }
-    
+
+    public static void main(String args[]){
+        caboos choo = new caboos();
+        caboos choochoo = new caboos();
+    }
 }
